c846703fb6f9cb15322c55a92861aeb352236838
xcode-vn <xcode.vn@hotmail.com>
2019-11-22 00:40:37 +0700

    [-]

--- vscode_build_helper.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd C:\GPRS_C_SDK
-echo Cleaning all
-powershell -command .\build.bat clean all
-echo Start building project '%1%'
-echo '--------------------------'
-powershell -command .\build.bat %1%
